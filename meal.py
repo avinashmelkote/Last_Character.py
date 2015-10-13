185e5cdaadc6d7f545a7d6a2bbb07ae44af34b64
avinashmelkote <avinashmelkote@gmail.com>
2015-10-13 14:05:19 -0400

    Update and rename README.md to meal.py
    Find the last characters.

---
+++ meal.py
@@ -0,0 +1,22 @@
+# Last_Character.py
+# Find the last characters of various items in a meal based on the lengths of the respective string.
+meal = 'pasta'
+vegetable = 'broccoli'
+fruit = 'strawberry'
+spice = 'turmeric'
+dessert = 'pudding'
+mealcount = len(meal)
+lastmeal = meal[mealcount-1]
+print lastmeal
+vegetablecount = len(vegetable)
+lastvegetable = vegetable[vegetablecount-1]
+print lastvegetable
+fruitcount = len(fruit)
+lastfruit = fruit[fruitcount-1]
+print lastfruit
+spicecount = len(spice)
+lastspice = spice[spicecount-1]
+print lastspice
+dessertcount = len(dessert)
+lastdessert = dessert[dessertcount-1]
+print lastdessert
